ca5cc5925b18a14b84dc3d439f89a146df1c2bd6
luiszn <luisdls124@gmail.com>
2025-09-27 17:03:22 -0500

    V2

--- POOgrupo5/src/poogrupo5/POOgrupo5.java
+++ POOgrupo5/src/poogrupo5/POOgrupo5.java
@@ -16,6 +16,7 @@ public class POOgrupo5 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
+        System.out.println("V2");
     }
     
 }
