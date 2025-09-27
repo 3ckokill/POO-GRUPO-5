8e3b1794a29301b34b41293f70b48a15c97aef6c
luiszn <luisdls124@gmail.com>
2025-09-27 18:09:13 -0500

    Vfinal

--- POOgrupo5/src/poogrupo5/POOgrupo5.java
+++ POOgrupo5/src/poogrupo5/POOgrupo5.java
@@ -4,6 +4,10 @@
  */
 package poogrupo5;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author luisd
@@ -13,12 +17,36 @@ public class POOgrupo5 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola mundo");
-        System.out.println("V2");
-        System.out.println("Nueva Rama");
-        System.out.println("linea 21");
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Persona> listaPersonas = new ArrayList<>();
+
+        try {
+            System.out.println("¿Cuántas personas desea ingresar?");
+            int n = sc.nextInt();
+            sc.nextLine(); 
+
+            
+
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Debe ingresar un número válido.");
+        } finally {
+            sc.close();
+        }
     }
-    
+}
+
+class Persona {
+    private String nombre;
+    private double sueldo;
+
+    public Persona(String nombre, double sueldo) {
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Sueldo: " + sueldo;
+    }
 }
