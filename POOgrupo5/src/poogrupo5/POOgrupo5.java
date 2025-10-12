8a752496d309b393d67e98288bf531c4366cb973
Jose <3ckokill1995@gmail.com>
2025-10-12 00:08:04 -0500

    agregando proyecto

--- POOgrupo5/src/poogrupo5/POOgrupo5.java
+++ POOgrupo5/src/poogrupo5/POOgrupo5.java
@@ -14,9 +14,7 @@ import java.util.Scanner;
  */
 public class POOgrupo5 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
      public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Persona> listaPersonas = new ArrayList<>();
---
+++ ProyectoImprenta/src/proyectoimprenta/Empleado.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoimprenta;
+
+/**
+ *
+ * @author 3ckok
+ */
+public class Empleado {
+    
+}
