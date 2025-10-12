83ab41b84fe5eb29a212f525d36bbe7ea08b1d21
Jose <3ckokill1995@gmail.com>
2025-10-12 01:31:25 -0500

    atributos empleado

--- ProyectoImprenta/src/proyectoimprenta/Empleado.java
+++ ProyectoImprenta/src/proyectoimprenta/Empleado.java
@@ -9,11 +9,12 @@ package proyectoimprenta;
  * @author 3ckok
  */
 public class Empleado extends Persona {
-    
+
     String sueldo;
     String cargo;
 
-    public Empleado(String sueldo, String cargo, String apellidoPaterno, String apellidoMaterno, String tipoDocumento, String NumeroDoumento, String dirrecion, String numeroCelular, String correo) {
+    public Empleado(String sueldo, String cargo, String apellidoPaterno, String apellidoMaterno, String tipoDocumento,
+            String NumeroDoumento, String dirrecion, String numeroCelular, String correo) {
         super(apellidoPaterno, apellidoMaterno, tipoDocumento, NumeroDoumento, dirrecion, numeroCelular, correo);
         this.sueldo = sueldo;
         this.cargo = cargo;
@@ -21,7 +22,18 @@ public class Empleado extends Persona {
 
     @Override
     public void mostrarDatos() {
-        
+
+        System.out.println("Empleado\n");
+        System.out.println("Nombres y Apellidos: " + this.nombreCompleto + " " + this.apellidoPaterno + " "
+                + this.apellidoMaterno);
+        System.out.println("Tipo de Documento: " + this.tipoDocumento);
+        System.out.println("Numero de Documento: " + this.NumeroDoumento);
+        System.out.println("Dirrecion: " + this.dirrecion);
+        System.out.println("Numero de Celular: " + this.numeroCelular);
+        System.out.println("Correo: " + this.correo);
+        System.out.println("Sueldo: " + this.sueldo);
+        System.out.println("Cargo: " + this.cargo);
+        System.out.println("\n");
     }
-    
+
 }
